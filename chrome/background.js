45d83b19340d60b3a36e62ae817652dce9fdf2fb
perguto <perguto@protonmail.com>
2022-07-27 22:34:38 +0200

    action now working

--- chrome/background.js
+++ chrome/background.js
@@ -1,13 +1,34 @@
 console.log("BACKGROUND\nSCRIPT\nSTARTING")
 	let tabid;
 
+let acivate_content_script = message => {
+	console.log(`sending "${message}" to content-script`);
+	chrome.tabs.query({active:true,lastFocusedWindow:true},x=>{console.log(...x);tabid=x[0].id;
+		console.log(tabid);
+		chrome.tabs.sendMessage(tabid, {text:message})
+	})
+}
+
 
 chrome.commands.onCommand.addListener(command => {
 	console.log(`COMMAND "${command}" triggered.`);
-	chrome.tabs.query({active:true,lastFocusedWindow:true},x=>{console.log(...x);tabid=x[0].id;
-		console.log(tabid);
-		chrome.tabs.sendMessage(tabid, {text:'screenshot'})
-	});
+	acivate_content_script("screenshot")
+//	chrome.tabs.query({active:true,lastFocusedWindow:true},x=>{console.log(...x);tabid=x[0].id;
+//		console.log(tabid);
+//		chrome.tabs.sendMessage(tabid, {text:'screenshot'})
+//	});
 	//chrome.tabs.query({},x=>console.log(...x));
 	//fullScreenshot();
 })
+
+chrome.action.onClicked.addListener(tab => {
+	console.log(tab);
+	acivate_content_script("action")
+    // Send a message to the active tab
+//    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//      var activeTab = tabs[0];
+//		chrome.tabs.sendMessage(tabid, {text:'action'})
+//    });
+  });
+
+
